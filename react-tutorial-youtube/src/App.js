e6b2e694f94fc9b1d0b49de348378527f22cac94
DaijiroNozaki-afk <nozaki@serge-gens.jp>
2023-02-15 15:17:30 +0900

    react-tutorial-youtube was finished.

--- react-tutorial-youtube/src/App.js
+++ react-tutorial-youtube/src/App.js
@@ -4,12 +4,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name: "Todo1", completed: false },
   ]);
   const todoNameRef = useRef();
   const handleAddTodo = () => {
 //タスクを追加する
     const name = todoNameRef.current.value;
+    if (name === "") return;
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false}];
     });
@@ -19,15 +19,19 @@ function App() {
     const newTodos = [...todos];
     const todo = newTodos.find((todo) => todo.id === id);
     todo.completed = !todo.completed;
-    setTodos(newTodos)
+    setTodos(newTodos);
   };
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク：0</div>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
